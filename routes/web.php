487ab85d303bd2403c7e8eb3dd09fd6882b090a6
Amigo20057 <denrizuk@gmail.com>
2025-06-20 16:28:49 +0300

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\BookingController;
 use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/cars');
 
 Route::get('/cars', [CarController::class, 'index']);
 Route::get('/calendar', [CarController::class, 'calendar'])->name("calendar");
